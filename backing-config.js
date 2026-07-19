f6f2fccf2a83eec17ebba570de5963c44b4a5ec6
gray5508 <hxf5508@gmail.com>
2026-07-19 20:16:47 +0800

    Update shared backing segment timings

--- backing-config.js
+++ backing-config.js
@@ -1,13 +1,47 @@
 (function () {
   "use strict";
-
-  // 统一配置文件：由页面中的“导出统一配置”生成并替换此文件。
-  // 提交到 GitHub 后，未做本机修改的电脑、手机和平板会读取同一套参数。
   window.DRUM_BACKING_CONFIG = {
-    version: 1,
-    revision: "",
-    exportedAt: "",
-    globalRate: 1,
-    segments: {}
-  };
+  "version": 1,
+  "revision": "20260719121315",
+  "exportedAt": "2026-07-19T12:13:15.478Z",
+  "globalRate": 1,
+  "segments": {
+    "system-1-region-3": {
+      "start": 55,
+      "end": 94.4,
+      "rate": 1,
+      "holdAction": "video"
+    },
+    "system-3-region-1": {
+      "start": 87.5,
+      "end": 117.3,
+      "rate": 1,
+      "holdAction": "backing"
+    },
+    "system-5-region-1": {
+      "start": 114.6,
+      "end": 131.3,
+      "rate": 1,
+      "holdAction": "backing"
+    },
+    "system-6-region-1": {
+      "start": 129.9,
+      "end": 147.4,
+      "rate": 1,
+      "holdAction": "backing"
+    },
+    "system-7-region-1": {
+      "start": 145.3,
+      "end": 163.5,
+      "rate": 1,
+      "holdAction": "backing"
+    },
+    "system-8-region-1": {
+      "start": 160.7,
+      "end": 174,
+      "rate": 1,
+      "holdAction": "backing"
+    }
+  }
+};
 })();
